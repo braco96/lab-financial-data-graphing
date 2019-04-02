5bb559606030de2f261025b5f094a523b46cf8bd
braco96 <bravocolladoluis@gmail.com>
2019-04-02 16:30:00 +0000

    commit v0.01

---
+++ public/js/financial-data.js
@@ -0,0 +1,64 @@
+// Este script carga y representa los datos históricos de Bitcoin
+
+// Guardamos la instancia de la gráfica para poder actualizarla más adelante
+let bitcoinChart;
+
+// Función principal que recupera los datos y actualiza la gráfica
+function loadBitcoinData() {
+  // Tomamos los valores de los filtros establecidos por el usuario
+  const from = document.getElementById('from').value;
+  const to = document.getElementById('to').value;
+  const currency = document.getElementById('currency').value;
+
+  // Construimos la URL de la API incluyendo los filtros necesarios
+  let apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}`;
+  if (from && to) {
+    apiUrl += `&start=${from}&end=${to}`; // añadimos fechas solo si ambas existen
+  }
+
+  // Realizamos la petición a la API con axios
+  axios
+    .get(apiUrl)
+    .then(response => {
+      // Obtenemos etiquetas (fechas) y valores (precios) de la respuesta
+      const labels = Object.keys(response.data.bpi);
+      const data = Object.values(response.data.bpi);
+
+      // Si ya teníamos una gráfica la eliminamos para evitar superposiciones
+      if (bitcoinChart) bitcoinChart.destroy();
+
+      // Creamos la gráfica de líneas con los datos obtenidos
+      bitcoinChart = new Chart(
+        document.getElementById('myChart').getContext('2d'),
+        {
+          type: 'line',
+          data: {
+            labels,
+            datasets: [
+              {
+                label: `Bitcoin (${currency})`,
+                data,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)'
+              }
+            ]
+          }
+        }
+      );
+
+      // Calculamos y mostramos los valores máximo y mínimo de la serie
+      const max = Math.max(...data);
+      const min = Math.min(...data);
+      document.getElementById('max').innerText = max.toFixed(2);
+      document.getElementById('min').innerText = min.toFixed(2);
+    })
+    .catch(err => console.error('Error al cargar datos:', err));
+}
+
+// Cargamos los datos iniciales al entrar en la página
+window.onload = loadBitcoinData;
+
+// Volvemos a cargar los datos cada vez que cambien los filtros
+document.getElementById('from').addEventListener('change', loadBitcoinData);
+document.getElementById('to').addEventListener('change', loadBitcoinData);
+document.getElementById('currency').addEventListener('change', loadBitcoinData);
---
+++ readme.md
@@ -0,0 +1,32 @@
+@@ -121,25 +121,32 @@ As in the dates filter, the data has to be reloaded every time the currency chan
+
+**Tasks**
+
+- Add a dropdown to indicate which currency will be shown in the application.
+- Add a listener to the `select` field to reload the data every time its value changes.
+- Add the currency to the API URL to load the correct values in our application.
+
+<br>
+
+## Iteration #5 (Bonus): Max/Min Values
+
+To finish up with the exercise, let\'s add an information box where we are going to show which are the maximum and minimum values in the indicated filters.
+
+To do that, we have to get the prices from the response we got in the first iteration and select the max and min values. Once we have these values, we have to show them in the view.
+
+**Tasks**
+
+- Add an information box in the HTML to show the max/min values from the chart.
+- Create an array with the price values from the response.
+- Use `Math.min()` and `Math.max()` to filter the array and get the min/max values.
+- Show these values in the information box we have added to the view.
+
+<br><br>
+
+**Happy coding!** :heart:
+
+## Implementación realizada
+
+- Añadidos enlaces CDN de Axios y Chart.js para consumir la API y dibujar la gráfica.
+- Creado formulario con filtros de fecha y divisa para consultar el precio del Bitcoin.
+- Script `financial-data.js` que obtiene los datos de CoinDesk, genera la gráfica y muestra valores máximo y mínimo.
